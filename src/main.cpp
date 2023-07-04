ba801fd268f8a78ffeb0b45842712aacad64511a
Adrianna CARNEC <adcarnec@paul-f5Ar3s2.clusters.42paris.fr>
2023-07-04 13:37:33 +0200

    exemple dir listing

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,28 @@
 #include "Config.hpp"
 #include "WebServ.hpp"
 
+
+
+#include <dirent.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <stdio.h>
+void printDir() {
+    DIR             *dir;
+    struct dirent   *entry;
+
+    if ((dir = opendir("./data/default")) == NULL)
+        perror("opendir() error");
+    else {
+        printf("contents of ./data/default:\n");
+        while ((entry = readdir(dir)) != NULL)
+            printf("  %s\n", entry->d_name);
+        printf("\n");
+        closedir(dir);
+    }
+}
+
+
 int main(int ac, char** av)
 {
     if (ac == 1)
@@ -13,11 +35,14 @@ int main(int ac, char** av)
         return(-1);
     }
 
+    printDir(); // TODO delete
+
     if (!Config::isValid())
         return(-1);
 
     if (!WebServ::runListeners())
         return(-1);
+
         
     WebServ::stop();
     return 0;
