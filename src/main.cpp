8b7f5951d8bd342b8d9cd9fc11870c278912fda5
Adrianna CARNEC <adcarnec@paul-f5Br1s1.clusters.42paris.fr>
2023-07-26 10:46:06 +0200

    feat: prep epoll

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adcarnec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 10:42:20 by adcarnec          #+#    #+#             */
+/*   Updated: 2023/07/26 10:42:28 by adcarnec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include "Config.hpp"
 #include "WebServ.hpp"
 
 int main(int ac, char** av)
 {
+    // TODO del qd epoll ok
+    std::string arg;
+    
     if (ac == 1)
-        Config::init("./conf/default.conf");
+        Config::init("./conf/default.conf"); // Loading default configuration file
     else if (ac == 2)
-        Config::init(av[1]);
+    {
+        // TODO del qd epoll ok
+        arg = av[1];
+        if (arg == "EPOLL")
+        {
+            Config::init("./conf/default.conf");
+        }
+        else
+            Config::init(av[1]); // Loading specified  configuration file
+    }
     else {
         std::cout << "Error: too many arguments" << std::endl;
         return(-1);
@@ -16,8 +40,16 @@ int main(int ac, char** av)
     if (!Config::isValid())
         return(-1);
 
-    if (!WebServ::runListeners())
-        WebServ::stop();
+    // TODO del qd epoll ok
+    if (arg == "EPOLL")
+    {
+        std::cout << "EPOLL" << std::endl;
+    }
+    else
+    {
+        if (!WebServ::runListeners())
+                WebServ::stop();
+    } 
 
     return 0;
 }
