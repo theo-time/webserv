dc849fec7ed48bfdd4fc0c9e1d296bac09c8902c
Adrianna CARNEC <adcarnec@paul-f5Br1s1.clusters.42paris.fr>
2023-07-26 10:31:45 +0200

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,6 @@
 #include "Config.hpp"
 #include "WebServ.hpp"
 
-/* 
-
-#include <dirent.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <stdio.h>
-void printDir() {
-    DIR             *dir;
-    struct dirent   *entry;
-
-    if ((dir = opendir("./data/default")) == NULL)
-        perror("opendir() error");
-    else {
-        printf("contents of ./data/default:\n");
-        while ((entry = readdir(dir)) != NULL)
-            printf("  %s\n", entry->d_name);
-        printf("\n");
-        closedir(dir);
-    }
-}
- */
-
 int main(int ac, char** av)
 {
     if (ac == 1)
